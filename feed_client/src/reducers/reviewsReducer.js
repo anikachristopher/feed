759b1bef145697a5354fad6f3ab5105554493e6f
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-26 20:33:56 -0400

    update reviews reducer

--- feed_client/src/reducers/reviewsReducer.js
+++ feed_client/src/reducers/reviewsReducer.js
@@ -2,6 +2,8 @@ export const reviewsReducer = (state = [], action) => {
     switch(action.type){
         case 'FETCH_REVIEWS':
             return action.payload
+        case 'CREATE_REVIEW':
+            return [...state, action.payload]
         default:
             return state
     }
