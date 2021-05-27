f85d1ffa6b009738de066e44ff204fe1310dbc4c
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-26 20:33:12 -0400

    built a create for the review

--- feed_client/src/actions/reviewsActions.js
+++ feed_client/src/actions/reviewsActions.js
@@ -2,7 +2,22 @@ export const getAllReviews = () => {
     return (dispatch) => {
         fetch('http://127.0.0.1:3000/reviews')
         .then(resp => resp.json())
-        .then(reviews => console.log(reviews))
+        .then(reviews => dispatch({ type: 'FETCH_REVIEWS', payload: reviews}))
     }
 
+}
+
+
+//build CRUD functionality here - create and delete (maybe update)
+
+export const createReview = (review) => {
+    return dispatch => {
+        fetch('http://127.0.0.1:3000/reviews', {
+            method: 'POST',
+            body: JSON.stringify(review),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        .then(resp => resp.json())
+        .then(review => dispatch({ type: 'ADD_REVIEW', payload: review}))
+    }
 }
