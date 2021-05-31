79b5794a4a561a2bd22e68014e3e775ac9605ffa
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-30 22:52:22 -0400

    built routes

---
+++ feed_client/src/components/Pricing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pricing = () => {
+        return (
+            <div>
+                Pricing List for Services
+            </div>
+        );
+
+};
+
+export default Pricing;
