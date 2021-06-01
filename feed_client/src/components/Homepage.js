018e17928898145c00011a1450717aab1f63a765
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-31 21:54:16 -0400

    created navbar and added styling to homepage

--- feed_client/src/components/Homepage.js
+++ feed_client/src/components/Homepage.js
@@ -1,12 +1,30 @@
+//route
 import React from 'react';
 
 const Homepage = () => {
         return (
-            <div>
-                Homepage
+            <div
+            style={{
+              flexwrap: 'wrap',
+              aligncontent: 'space-evenly',
+              justifyContent: 'right',
+              alignItems: 'right',
+              height: '90vh',
+            }}
+            >
+                <h4>Welcome to FEED! where we value our customers feedback</h4><br></br>
+                <h4>We've been in business for 25 years</h4><br></br>
+                <h4>Amazing customer service has been at the forefront of our priorities from Day 1.</h4><br></br>
+                <h4>Take some time to learn about our service offerings!</h4>
+                <hr />
             </div>
+
+            
         );
 
 };
 
 export default Homepage;
+
+//should be done
+//stateless component
--- feed_client/src/components/Navbar.js
+++ feed_client/src/components/Navbar.js
@@ -26,4 +26,66 @@ const NavBar = () => {
 
 export default NavBar;
 
+// <div class="container">
+//             <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+//                   <div class="w3-container w3-display-container w3-padding-16">
+//                     <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+//                     <h3 class="w3-wide"><b>FEED</b></h3>
+//                   </div>
+//                   <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+//                     <a href="#" class="w3-bar-item w3-button">HOMEPAGE</a>
+//                     <a href="#" class="w3-bar-item w3-button">CUSTOMER SERVICE</a>
+//                     <a href="#" class="w3-bar-item w3-button">INSTALLATIONS</a>
+//                     <a href="#" class="w3-bar-item w3-button">REPAIRS</a>
+//                     <a href="#" class="w3-bar-item w3-button">REVIEWS</a>
+//                     <a href="#" class="w3-bar-item w3-button">Contact Us</a>
+//                     <a onclick="" href="javascript:void(0)" class="w3-button w3-block w3-white w3-center-align" id="myBtn">
+//             </nav>
+//         </div>
+
 //navbar is up but not connected to the routes
+
+
+
+// function Navbar() {
+
+//     const [click, setClick] = useState(false);
+
+//     const handleClick = () => setClick(!click);
+//     const closeMobileMenu = () => setClick(false);
+
+//     const showButton = () => {
+//         if (window.innerWidth <= 960) {
+//           setButton(false);
+//         } else {
+//           setButton(true);
+//         }
+//     };
+    
+//     useEffect(() => {
+//         showButton();
+//       }, []);
+    
+//       window.addEventListener('resize', showButton);
+
+//     return (
+//         <nav class="w3-sidebar w3-bar-block w3-white w3-collapse w3-top" style="z-index:3;width:250px" id="mySidebar">
+//         <div class="w3-container w3-display-container w3-padding-16">
+//           <i onclick="w3_close()" class="fa fa-remove w3-hide-large w3-button w3-display-topright"></i>
+//           <h3 class="w3-wide"><b>FEED</b></h3>
+//         </div>
+//         <div class="w3-padding-64 w3-large w3-text-grey" style="font-weight:bold">
+//           <a href="#" class="w3-bar-item w3-button">HOMEPAGE</a>
+//           <a href="#" class="w3-bar-item w3-button">CUSTOMER SERVICE</a>
+//           <a href="#" class="w3-bar-item w3-button">INSTALLATIONS</a>
+//           <a href="#" class="w3-bar-item w3-button">REPAIRS</a>
+//           <a href="#" class="w3-bar-item w3-button">REVIEWS</a>
+//           <a href="#" class="w3-bar-item w3-button">Contact Us</a>
+//           <a onclick="" href="javascript:void(0)" class="w3-button w3-block w3-white w3-center-align" id="myBtn">
+//         </nav>
+
+        
+//     )
+// }
+
+// export default Navbar
