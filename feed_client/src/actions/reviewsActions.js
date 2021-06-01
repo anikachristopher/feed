53de3efdb46b6d5d708671c9f27fe0061dc5cab1
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-31 21:55:34 -0400

    built delete CRUD functionality

--- feed_client/src/actions/reviewsActions.js
+++ feed_client/src/actions/reviewsActions.js
@@ -2,15 +2,20 @@ export const getAllReviews = () => {
     return (dispatch) => {
         fetch('http://127.0.0.1:3000/reviews')
         .then((resp) => {
-            resp.json();
+            return resp.json();
         })
         .then((reviews) => {
-            console.log(reviews);
-        // dispatch({ type: 'GET_ALL_REVIEWS', payload: reviews});
+            // toDispatch(dispatch, reviews);
+            // console.log(reviews);
+        dispatch({ type: 'GET_ALL_REVIEWS', payload: reviews});
         }
         )}
 }
     
+// function toDispatch(dispatch, daReviews){
+//     dispatch({ type: 'GET_ALL_REVIEWS', payload: daReviews});
+// }
+
 
 //build CRUD functionality here - create and delete (maybe update)
 
@@ -23,5 +28,17 @@ export const createReview = (review) => {
         })
         .then(resp => resp.json())
         .then(review => dispatch({ type: 'CREATE_REVIEW', payload: review}))
+    }
+}
+
+export const deleteReview = review => {
+    return dispatch => {
+        dispatch({type: 'DELETE_REVIEW', payload: review.id})
+        fetch(`http://localhost:3000/reviews/${review.id}`, {
+            method: 'DELETE',
+            body: JSON.stringify(review),
+            headers: { 'Content-Type': 'application/json'}
+        })
+        
     }
 }
