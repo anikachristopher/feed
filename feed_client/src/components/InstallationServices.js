ce90ca93618db2355c636bbfee9375659b57ab29
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-31 21:54:47 -0400

    built installation component

--- feed_client/src/components/InstallationServices.js
+++ feed_client/src/components/InstallationServices.js
@@ -5,7 +5,9 @@ import React from 'react';
 const InstallationServices= () => {
         return (
             <div>
-                You're in installation service.
+                <h4>You're in installation service.</h4>
+               <h4>Basically will display all the installation services</h4> 
+                <h4>Then says contact us with any questions (through the form)</h4>
                
                 <hr />
             </div>
@@ -15,3 +17,5 @@ const InstallationServices= () => {
 };
 
 export default InstallationServices;
+
+//not showing up for some reason. not working.
