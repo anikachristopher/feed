28462a63869b02440e40e760635c0aa1d3dbc060
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-31 21:56:20 -0400

    styling and connected routes

--- feed_client/src/index.js
+++ feed_client/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { render } from 'react-dom';
 import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
-import { rootReducer } from './reducers/rootReducer'
+import { rootReducer } from './reducers/rootReducer';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
