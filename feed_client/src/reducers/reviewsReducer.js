5fe5fea2da700f9b683982f16bbdb48007a07eab
Anika Christopher <anika.mchristopher@gmail.com>
2021-05-31 21:52:30 -0400

    added delete action

--- feed_client/src/reducers/reviewsReducer.js
+++ feed_client/src/reducers/reviewsReducer.js
@@ -1,9 +1,11 @@
 export const reviewsReducer = (state = [], action) => {
     switch(action.type){
-        case 'FETCH_REVIEWS':
+        case 'GET_ALL_REVIEWS':
             return action.payload
         case 'CREATE_REVIEW':
             return [...state, action.payload]
+        case 'DELETE_REVIEW':
+            return  state.filter(review => review.id !== action.payload )
         default:
             return state
     }
