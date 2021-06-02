44248c377b7b195c178bdbf714356e2d103e7f2f
Anika Christopher <anika.mchristopher@gmail.com>
2021-06-02 00:41:41 -0400

    created a contact route

---
+++ feed_client/src/components/Contact.js
@@ -0,0 +1,31 @@
+//route
+import React from 'react';
+
+const Contact = () => {
+        return (
+            <div
+            // style={{
+            //   flexwrap: 'wrap',
+            //   aligncontent: 'space-evenly',
+            //   justifyContent: 'right',
+            //   alignItems: 'right',
+            //   height: '90vh',
+            // }}
+            >
+                <h4>Address: 123 Flatiron Street, New York, NY 10013</h4>
+                <h4>Phone: 1-800-555-0429</h4>
+                <h4>Website: www.feed.com</h4>
+                <h4>Email: service@feed.com</h4>
+                
+                
+            </div>
+
+            
+        );
+
+};
+
+export default Contact;
+
+//should be done
+//stateless component
