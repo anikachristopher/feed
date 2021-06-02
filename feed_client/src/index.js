2fbc3aaf73d83271637041fedd47b00fb6d9dd93
Anika Christopher <anika.mchristopher@gmail.com>
2021-06-02 00:39:10 -0400

    updated the imports

--- feed_client/src/index.js
+++ feed_client/src/index.js
@@ -6,7 +6,7 @@ import thunk from 'redux-thunk';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import App from './components/App';
+import App from './App';
 import { rootReducer } from './reducers/rootReducer';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
